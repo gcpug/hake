b956d708f5fcaf9d2715688b62493f97e13f121e
Takuya Ueda <uedatakuya@gmail.com>
2019-01-24 18:55:14 +0900

    Add Row and Rows

---
+++ row_test.go
@@ -0,0 +1,65 @@
+package sgcvj_test
+
+import (
+	"testing"
+
+	"cloud.google.com/go/spanner"
+	"github.com/sinmetal/sgcvj"
+)
+
+func TestRow_MarshalJSON(t *testing.T) {
+
+	cases := []struct {
+		name string
+		row  *spanner.Row
+		want string
+	}{
+		{"null", row(t, nil), toJSON(t, nil)},
+		{"empty", row(t, R{}), toJSON(t, R{})},
+		{"single", row(t, R{"col1": 100}), toJSON(t, R{"col1": 100})},
+		{"multiple", row(t, R{"col1": 100, "col2": 10.5}), toJSON(t, R{"col1": 100, "col2": 10.5})},
+	}
+
+	for _, tt := range cases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				if r := recover(); r != nil {
+					t.Errorf("unexpected panic: %v", r)
+				}
+			}()
+			got := toJSON(t, (*sgcvj.Row)(tt.row))
+			if got != tt.want {
+				t.Errorf("want %s but got %s", tt.want, got)
+			}
+		})
+	}
+}
+
+func TestRows(t *testing.T) {
+
+	cases := []struct {
+		name string
+		rows []*spanner.Row
+		want string
+	}{
+		{"null", rows(t, nil), toJSON(t, nil)},
+		{"empties", rows(t, []R{{}, {}}), toJSON(t, []R{{}, {}})},
+		{"singles", rows(t, []R{{"col1": 100}, {"col2": 10.5}}), toJSON(t, []R{{"col1": 100}, {"col2": 10.5}})},
+	}
+
+	for _, tt := range cases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				if r := recover(); r != nil {
+					t.Errorf("unexpected panic: %v", r)
+				}
+			}()
+			got := toJSON(t, sgcvj.Rows(tt.rows))
+			if got != tt.want {
+				t.Errorf("want %s but got %s", tt.want, got)
+			}
+		})
+	}
+}
